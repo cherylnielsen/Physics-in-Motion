2aaa04f2afe465f3181acca571a91bf134a8b848
Cheryl R. Nielsen <cnielsen@mail.sfsu.edu>
2022-10-16 22:46:26 -0700

    In game world UI canvas black boards interactive

--- PhysicsInMotion/Assets/Scripts/GUI Scripts/DigitalClock.cs
+++ PhysicsInMotion/Assets/Scripts/GUI Scripts/DigitalClock.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
+
+/***
+ * Class for active digital clock for use in Lab and Lobby GUIs.
+ * Displays date and time for students and updates time every frame.
+ * Uses local time.
+ * 
+ * author:  Cheryl Nielsen
+ * date:    Oct 16, 2022
+ * version: 1.0
+ * 
+ */
 public class DigitalClock : MonoBehaviour
 {
+    private TextMeshProUGUI timeText;
+
+    // Awake is called when script is first loaded,
+    // or attached object is instantiated, 
+    // even if the script is disabled.
+    void Awake()
+    {
+        timeText = GetComponent<TextMeshProUGUI>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +37,7 @@ public class DigitalClock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DateTime time = DateTime.Now;
+        timeText.text = time.ToString();
     }
 }
